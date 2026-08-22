95784761b4a7b64bda27ff8efe4140c8f41ac03a
adriantwill <142526523+adriantwill@users.noreply.github.com>
2026-08-22 11:13:48 -0400

    load model

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
 import pandas as pd
+from torch import Tensor
 from torch.utils.data import DataLoader, Dataset
 from torchvision.io import decode_image
+from transformers import (
+    AutoProcessor,
+    BitsAndBytesConfig,
+    Qwen3VLForConditionalGeneration,
+)
+
+def collate_fn(batch: list[Tensor, str]):
+
 
 
 class CustomDataset(Dataset):
     def __init__(self, transform=None, target_transform=None):
-        self.dataset = pd.read_csv("data/sql/questions_rows.csv")
+        self.dataset = pd.read_csv("data/sql/temp_questions.csv")
         self.img_dir = "data/slide_images"
         self.transform = transform
         self.target_transform = target_transform
@@ -15,9 +24,10 @@ class CustomDataset(Dataset):
 
     def __getitem__(self, idx):
         row = self.dataset.iloc[idx]
-        img = f"data/images/{row['id']} - {row['page_number']}"
+        print(row["storage_path"])
+        img = f"data/pdf_images/{row['storage_path']}_{int(row['page_number'])}.png"
         img = decode_image(img)
-        label = row["question"]
+        label = row["question_text"]
         if self.transform:
             img = self.transform(img)
         if self.target_transform:
@@ -25,8 +35,14 @@ class CustomDataset(Dataset):
         return img, label
 
 
+quantization_config = BitsAndBytesConfig(
+    load_in_4bit=True,
+)
 dataset = CustomDataset()
-dataloader = DataLoader(dataset, batch_size=32, shuffle=True)
-train_features, train_labels = next(iter(dataloader))
-print(f"Feature batch shape: {train_features.size()}")
-print(f"Labels batch shape: {train_labels.size()}")
+
+dataloader = DataLoader(dataset, batch_size=1, shuffle=True)
+model = Qwen3VLForConditionalGeneration.from_pretrained(
+    "Qwen/Qwen3-VL-8B-Instruct",
+    quantization_config=quantization_config,
+)
+processor = AutoProcessor.from_pretrained("Qwen/Qwen3-VL-8B-Instruct")
---
+++ util/remove_questions_without_pdfs.py
@@ -0,0 +1,24 @@
+from pathlib import Path
+
+import pandas as pd
+
+PROJECT_ROOT = Path(__file__).resolve().parents[1]
+UPLOADS_CSV = PROJECT_ROOT / "data/sql/uploads_rows.csv"
+QUESTIONS_CSV = PROJECT_ROOT / "data/sql/questions_rows.csv"
+PDF_DIRECTORY = PROJECT_ROOT / "data/pdfs"
+
+if __name__ == "__main__":
+    questions = pd.read_csv(QUESTIONS_CSV)
+    uploads = pd.read_csv(UPLOADS_CSV)
+    files = [item.name for item in PDF_DIRECTORY.iterdir() if item.is_file()]
+    uploads["storage_path"] = uploads["storage_path"].str.replace(
+        "4nypRh2sNfV73kllqTMWoG0v0Z5zxi7f/", ""
+    )
+    uploads = uploads[uploads["storage_path"].isin(files)]
+    questions = questions[questions["upload_id"].isin(uploads["id"].tolist())]
+    questions = questions.dropna(subset=["page_number"])
+    questions["storage_path"] = questions["upload_id"].map(
+        uploads.set_index("id")["storage_path"]
+    )
+    questions["storage_path"] = questions["storage_path"].str.replace(".pdf", "")
+    questions.to_csv("data/sql/temp_questions.csv", index=False)
